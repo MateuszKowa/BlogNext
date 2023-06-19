005a4933939241d801f20912636318bcbb70f896
Mateusz Kowa <m.kowa2424@gmail.com>
2023-06-19 19:12:01 +0200

    next try with typecript

--- blog_app/app/page.tsx
+++ blog_app/app/page.tsx
@@ -12,10 +12,24 @@ const query = gql`
     }
   }
 `;
-
+type postObject = {
+  title: string;
+  content: {
+    document: [children: [{ text: string }]];
+  };
+};
 export default async function Page() {
   const { data } = await getClient().query({ query });
-  console.log("CONCOLE", data?.posts[0]?.title);
+  console.log("CONCOLE", data?.posts[0]);
 
-  return <main>{data?.posts[0]?.title}</main>;
+  return (
+    <main>
+      {data?.posts?.map((value: postObject, index: number) => (
+        <div className="bg-green-400 my-10" key={index}>
+          <h1 key={index}>{value?.title}</h1>
+          <div>{value?.content?.document[0]?.children[0]?.text}</div>
+        </div>
+      ))}
+    </main>
+  );
 }
